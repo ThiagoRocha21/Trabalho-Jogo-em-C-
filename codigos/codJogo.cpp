b38afddfef0820465c3123121cff1eac9766dfd4
thigas <thiaguinho.rocha2110@gmail.com>
2023-11-21 15:46:04 -0300

    ajeitando partes do cód do jv

--- codigos/codJogo.cpp
+++ codigos/codJogo.cpp
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
+#include <locale.h>
 
 #define MAX_PALAVRA 20
 #define MAX_ERROS 27
+#define MAX_PALAVRAS_BANCO 5
+
+char bancoPalavras[MAX_PALAVRAS_BANCO][MAX_PALAVRA] = {"JVGOSTOSO", "KUNK", "PATRICIA", "BIELGAY", "WALLACE"};
 
 char palavra[MAX_PALAVRA];
 char forca[MAX_PALAVRA];
 char erros[MAX_ERROS];
 
 void limparBuffer(char *buf, int tamanho) {
-    int i;
-    for (i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanho; i++) {
         buf[i] = 0;
     }
 }
@@ -25,6 +29,38 @@ char maiuscula(char c) {
     return islower(c) ? toupper(c) : c;
 }
 
+void escolherPalavraAleatoria() {
+    srand(time(NULL));
+    int indice = rand() % MAX_PALAVRAS_BANCO;
+    strcpy(palavra, bancoPalavras[indice]);
+}
+
+void escolherPalavraManualmente() {
+    printf("\nDigite uma palavra: ");
+    scanf("%19s", palavra);
+}
+
+void escolherOpcaoPalavra() {
+    int opcao;
+    printf("\nEscolha a opção:\n");
+    printf("1. PLAYER VS CPU\n");
+    printf("2. PLAYER VS PLAYER\n");
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            escolherPalavraAleatoria();
+            break;
+        case 2:
+            escolherPalavraManualmente();
+            break;
+        default:
+            printf("Opção inválida. Escolhendo palavra do banco.\n");
+            escolherPalavraAleatoria();
+    }
+}
+
 void exibirForca() {
     printf("\n%s\n", forca);
 }
@@ -35,33 +71,44 @@ void exibirErros() {
     }
 }
 
-void start(void) {
-    limparBuffer(palavra, MAX_PALAVRA);
-    limparBuffer(forca, MAX_PALAVRA);
-    limparBuffer(erros, MAX_ERROS);
-
-    printf("\nDigite uma palavra: ");
-    scanf("%19s", palavra);
-
-    int i;
-    for (i = 0; palavra[i] != 0; i++) {
+void inicializarForca() {
+    for (int i = 0; palavra[i] != 0; i++) {
         char c = palavra[i];
         forca[i] = ehLetra(c) ? '_' : c;
     }
 }
 
-int jogo(void) {
+void mostrarResultado(int resultado) {
+    printf("\n");
+    if (resultado == 0) {
+        printf("Você perdeu. A palavra era %s\n", palavra);
+    } else {
+        printf("Parabéns, você acertou! a palavra era: %s\n", palavra);
+    }
+}
+
+void start() {
+    limparBuffer(forca, MAX_PALAVRA);
+    limparBuffer(erros, MAX_ERROS);
+    
+    escolherOpcaoPalavra();
+    
+    inicializarForca();
+
+    printf("\nBem-vindo ao Jogo da Forca!\n");
+}
+
+int jogo() {
     char tentativa;
     int chances = 5;
 
     int letras = 0;
-    int i;
-    for (i = 0; palavra[i] != 0; i++) {
+    for (int i = 0; palavra[i] != 0; i++) {
         if (ehLetra(palavra[i])) letras++;
     }
 
     while (chances > 0) {
-        system("cls");
+        system("cls"); // Substitua por uma abordagem mais portátil, se necessário
         exibirForca();
         exibirErros();
 
@@ -72,7 +119,7 @@ int jogo(void) {
         if (!ehLetra(tentativa)) continue;
 
         int jaTentou = 0;
-        for (i = 0; erros[i] != 0; i++) {
+        for (int i = 0; erros[i] != 0; i++) {
             if (erros[i] == maiuscula(tentativa)) {
                 jaTentou = 1;
                 break;
@@ -83,7 +130,7 @@ int jogo(void) {
 
         int ganhou = 1;
         int achou = 0;
-        for (i = 0; palavra[i] != 0; i++) {
+        for (int i = 0; palavra[i] != 0; i++) {
             if (!ehLetra(palavra[i])) continue;
             if (forca[i] == '_') {
                 if (maiuscula(palavra[i]) == maiuscula(tentativa)) {
@@ -107,16 +154,8 @@ int jogo(void) {
     return 0;
 }
 
-void mostrarResultado(int resultado) {
-    printf("\n");
-    if (resultado == 0) {
-        printf("Você perdeu. A palavra era %s\n", palavra);
-    } else {
-        printf("Parabéns, você acertou a palavra %s\n", palavra);
-    }
-}
-
 int main() {
+	setlocale(0,"portuguese");
     start();
     int resultado = jogo();
     mostrarResultado(resultado);
